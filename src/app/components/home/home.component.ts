93db378f7ac25a84240a4dcd4931dd4b901df493
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2020-11-06 20:40:05 +0300

    Total Cases

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalDataSummary } from 'src/app/models/global-data';
 import { DataServiceService } from 'src/app/services/data-service.service';
 
 @Component({
@@ -8,17 +9,31 @@ import { DataServiceService } from 'src/app/services/data-service.service';
 })
 export class HomeComponent implements OnInit {
 
+  totalConfirmed = 0;
+  totalActive = 0;
+  totalDeaths = 0;
+  totalRecovered = 0;
+  globalData: GlobalDataSummary[];
   constructor(private dataService: DataServiceService) { }
 
   ngOnInit(): void {
     this.dataService.getGlobalData()
-    .subscribe(
-      {
-        next: (result) => {
-          console.log(result);
+      .subscribe(
+        {
+          next: (result) => {
+            console.log(result);
+            this.globalData = result;
+            result.forEach(cs => {
+              if (!Number.isNaN(cs.confirmed)) {
+                this.totalActive += cs.active
+                this.totalConfirmed += cs.confirmed
+                this.totalDeaths += cs.deaths
+                this.totalRecovered += cs.recovered
+              }
+            })
+          }
         }
-      }
-    )
+      )
   }
 
 }
