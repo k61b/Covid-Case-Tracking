140d936612ad6b51c624a2f3f0ec172a5595b481
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2020-11-06 18:35:11 +0300

    Data Service

---
+++ src/app/services/data-service.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+  private globalDataUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/11-05-2020.csv`;
+  constructor(private http: HttpClient) { }
+
+  getGlobalData() {
+    return this.http.get(this.globalDataUrl, {responseType: 'text'}).pipe(
+      map(result => {
+
+        let rows = result.split('\n');
+
+        rows.forEach(row => {
+          let cols = row.split(/,(?=\S)/);
+          console.log(cols);
+        })
+
+        return [];
+      })
+    )
+  }
+}
