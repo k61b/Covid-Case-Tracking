30083404bc3ba1ad18aab2983ffad06e3909a458
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2020-11-09 23:42:48 +0300

    Country Cases

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GoogleChartInterface } from 'ng2-google-charts';
+import { DateWiseData } from 'src/app/models/date-wise-data';
 import { GlobalDataSummary } from 'src/app/models/global-data';
 import { DataServiceService } from 'src/app/services/data-service.service';
 
@@ -15,15 +17,45 @@ export class CountriesComponent implements OnInit {
   totalActive = 0;
   totalDeaths = 0;
   totalRecovered = 0;
+  selectedCountryData: DateWiseData[];
+  dateWiseData;
+  lineChart: GoogleChartInterface = {
+    chartType: 'LineChart'
+  }
   constructor(private service: DataServiceService) { }
 
   ngOnInit(): void {
+
+    this.service.getDateWiseData().subscribe(
+      (result) => {
+        this.dateWiseData = result;
+        this.updateChart();
+      }
+    )
+
     this.service.getGlobalData().subscribe(result => {
       this.data = result;
       this.data.forEach(cs => {
         this.countries.push(cs.country);
       })
     })
+  }
+
+  updateChart() {
+    let dataTable = [];
+    dataTable.push(["Date" , 'Cases'])
+    this.selectedCountryData.forEach(cs=>{
+      dataTable.push([cs.date , cs.cases])
+    })
+
+    this.lineChart = {
+      chartType: 'LineChart',
+      dataTable: dataTable,
+      //firstRowIsData: true,
+      options: {
+        height: 500
+      },
+    };
   }
 
   updateValues(country: string) {
@@ -36,6 +68,9 @@ export class CountriesComponent implements OnInit {
         this.totalRecovered = cs.recovered;
       }
     })
+
+    this.selectedCountryData = this.dateWiseData[country]
+    this.updateChart();
   }
 
 }
